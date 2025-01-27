e3f0195521b7b3a31fe96684f6a32ae17c91575a
tulm <tulmse184390@fpt.edu.vn>
2025-01-27 14:28:04 +0700

    Add Repository pattern

---
+++ VaccineScheduleTracking.API/Repository/IAccountRepository.cs
@@ -0,0 +1,9 @@
+﻿using VaccineScheduleTracking.API.Models.Entities;
+
+namespace VaccineScheduleTracking.API.Repository
+{
+    public interface IAccountRepository 
+    {
+        Task<Account?> GetAccountByUsernameAsync(string username);
+    }
+}
---
+++ VaccineScheduleTracking.API/Repository/SQLAccountRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VaccineScheduleTracking.API.Data;
+using VaccineScheduleTracking.API.Models.Entities;
+
+namespace VaccineScheduleTracking.API.Repository
+{
+    public class SQLAccountRepository : IAccountRepository
+    {
+        private readonly VaccineScheduleDbContext dbContext;
+
+        public SQLAccountRepository(VaccineScheduleDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<Account?> GetAccountByUsernameAsync(string username)
+        {
+            return await dbContext.Accounts.FirstOrDefaultAsync(user => user.Username == username);
+        }
+    }
+}
