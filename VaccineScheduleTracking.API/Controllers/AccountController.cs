ab85f43f257c43de98c9b4e371a285ac88ce0bb6
tulm <tulmse184390@fpt.edu.vn>
2025-01-27 14:29:58 +0700

    Add Login function

---
+++ VaccineScheduleTracking.API/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using VaccineScheduleTracking.API.Models.DTOs;
+using VaccineScheduleTracking.API.Repository;
+
+namespace VaccineScheduleTracking.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountRepository accountRepository;
+        private readonly IMapper mapper;
+
+        public AccountController(IAccountRepository accountRepository, IMapper mapper)
+        {
+            this.accountRepository = accountRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginAccountDto loginAccountDto)
+        {
+            var account = await accountRepository.GetAccountByUsernameAsync(loginAccountDto.Username);
+
+            if (account == null)
+            {
+                return Unauthorized(new { Message = "Account does not exist!" });
+            }
+
+            if (loginAccountDto.Password != account.Password)
+            {
+                return Unauthorized(new { Message = "Wrong account or password" });
+            }
+
+            return Ok(mapper.Map<AccountDto>(account));
+        }
+    }
+}
