66c7207b8b7f27137d51ae1890120687b116b07f
tulm <tulmse184390@fpt.edu.vn>
2025-01-27 14:28:51 +0700

    Add Automap for Account and Account's Dto

---
+++ VaccineScheduleTracking.API/Mappings/AutoMapperProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using VaccineScheduleTracking.API.Models.DTOs;
+using VaccineScheduleTracking.API.Models.Entities;
+
+namespace VaccineScheduleTracking.API.Mappings
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<Account, AccountDto>().ReverseMap();
+        }
+    }
+}
