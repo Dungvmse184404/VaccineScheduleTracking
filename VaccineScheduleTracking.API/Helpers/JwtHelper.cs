f71630fe1b2f25ff56f3802573a572dade0f7c5e
tulm <tulmse184390@fpt.edu.vn>
2025-01-27 15:14:06 +0700

    Add Jwt for authentication vs authorization

---
+++ VaccineScheduleTracking.API/Helpers/JwtHelper.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace VaccineScheduleTracking.API.Helpers
+{
+    public class JwtHelper
+    {
+        private readonly IConfiguration configuration;
+
+        public JwtHelper(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public string GenerateToken(string accountID, string role)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, accountID),
+                new Claim("Role", role),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: configuration["JwtSettings:Issuer"],
+                audience: configuration["JwtSettings:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(double.Parse(configuration["JwtSettings:ExpiresInMinutes"])),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
