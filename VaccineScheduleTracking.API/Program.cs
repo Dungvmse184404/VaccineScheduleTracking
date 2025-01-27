378c4abfd3c0abc2abe0bbdf4e9e028d522840d1
tulm <tulmse184390@fpt.edu.vn>
2025-01-27 14:07:09 +0700

    Inject dbcontext for application

--- VaccineScheduleTracking.API/Program.cs
+++ VaccineScheduleTracking.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using VaccineScheduleTracking.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<VaccineScheduleDbContext>(option =>
+option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
