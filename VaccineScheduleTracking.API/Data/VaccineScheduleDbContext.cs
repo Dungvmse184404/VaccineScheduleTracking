1d1c9bf3645c28085f6746a76c097974a8bf9975
tulm <tulmse184390@fpt.edu.vn>
2025-01-27 14:05:33 +0700

    Add system's DbContext

---
+++ VaccineScheduleTracking.API/Data/VaccineScheduleDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VaccineScheduleTracking.API.Models.Entities;
+
+namespace VaccineScheduleTracking.API.Data
+{
+    public class VaccineScheduleDbContext : DbContext
+    {
+        public VaccineScheduleDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) 
+        {
+            
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Parent> Parents { get; set; }
+        public DbSet<Child> Children { get; set; } 
+    }
+}
