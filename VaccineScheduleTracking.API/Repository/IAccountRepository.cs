6bc92cb7e57a586ee5b5eba84befbc57098fb9e3
tulm <tulmse184390@fpt.edu.vn>
2025-01-28 11:12:08 +0700

    Add more funtion to interact with database

--- VaccineScheduleTracking.API/Repository/IAccountRepository.cs
+++ VaccineScheduleTracking.API/Repository/IAccountRepository.cs
@@ -1,9 +1,15 @@
-﻿using VaccineScheduleTracking.API.Models.Entities;
+﻿using VaccineScheduleTracking.API.Models.DTOs;
+using VaccineScheduleTracking.API.Models.Entities;
 
 namespace VaccineScheduleTracking.API.Repository
 {
     public interface IAccountRepository 
     {
+        Task<Account?> GetAccountByID(int id);
         Task<Account?> GetAccountByUsernameAsync(string username);
+        Task<Account?> GetAccountByEmailAsync(string email);
+        Task<Account?> GetAccountByPhonenumberAsync(string phonenumber);
+        Task<Account> AddAsync(Account account);
+        Task<Account?> UpdateAsync(UpdateAccountDto updateAccount);
     }
 }
--- VaccineScheduleTracking.API/Repository/SQLAccountRepository.cs
+++ VaccineScheduleTracking.API/Repository/SQLAccountRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using VaccineScheduleTracking.API.Data;
+using VaccineScheduleTracking.API.Models.DTOs;
 using VaccineScheduleTracking.API.Models.Entities;
 
 namespace VaccineScheduleTracking.API.Repository
@@ -13,9 +14,51 @@ namespace VaccineScheduleTracking.API.Repository
             this.dbContext = dbContext;
         }
 
+        public async Task<Account?> GetAccountByEmailAsync(string email)
+        {
+            return await dbContext.Accounts.FirstOrDefaultAsync(user => user.Email == email);
+        }
+
+        public async Task<Account?> GetAccountByPhonenumberAsync(string phonenumber)
+        {
+            return await dbContext.Accounts.FirstOrDefaultAsync(user => user.PhoneNumber == phonenumber);
+        }
+
         public async Task<Account?> GetAccountByUsernameAsync(string username)
         {
             return await dbContext.Accounts.Include(x => x.Parent).FirstOrDefaultAsync(user => user.Username == username);
+        }
+
+        public async Task<Account> AddAsync(Account account)
+        {
+            await dbContext.Accounts.AddAsync(account);
+            await dbContext.SaveChangesAsync();
+
+            return account;
+        }
+
+        public async Task<Account?> GetAccountByID(int id)
+        {
+            return await dbContext.Accounts.FirstOrDefaultAsync(user => user.AccountID == id);
+        }
+
+        public async Task<Account?> UpdateAsync(UpdateAccountDto updateAccount)
+        {
+            var account = await GetAccountByID(updateAccount.AccountID);
+            if (account == null)
+            {
+                return null;
+            }
+            account.Firstname = updateAccount.Firstname;
+            account.Lastname = updateAccount.Lastname;
+            account.Email = updateAccount.Email;
+            account.PhoneNumber = updateAccount.PhoneNumber;
+            account.Avatar = updateAccount.Avatar;
+            account.Password = updateAccount.Password;
+
+            await dbContext.SaveChangesAsync();
+
+            return account;
         }
     }
 }
