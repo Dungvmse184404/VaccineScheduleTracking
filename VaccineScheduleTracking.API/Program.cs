a5a6251716a3c92cdd219a48fb2632dc0ef01b40
tulm <tulmse184390@fpt.edu.vn>
2025-01-28 11:12:49 +0700

    Config Swagger to accept Jwt Token

--- VaccineScheduleTracking.API/Program.cs
+++ VaccineScheduleTracking.API/Program.cs
@@ -6,6 +6,7 @@ using VaccineScheduleTracking.API.Data;
 using VaccineScheduleTracking.API.Helpers;
 using VaccineScheduleTracking.API.Mappings;
 using VaccineScheduleTracking.API.Repository;
+using VaccineScheduleTracking.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,11 +15,40 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    // Dinh nghia bear token
+    options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Description = "Enter 'Bearer {token}'.Ex: 'Bearer abc123xyz'"
+    });
+
+    // Yeu cau su dung token cho cac Api
+    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 builder.Services.AddDbContext<VaccineScheduleDbContext>(option =>
 option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IAccountRepository, SQLAccountRepository>();
+builder.Services.AddScoped<IAccountService, AccountService>();
 
 builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
 
