2811ec5ffa5c80ba94d945f8e2723f56a3948635
tulm <tulmse184390@fpt.edu.vn>
2025-01-28 11:10:30 +0700

    Create more Dto for Class Account

---
+++ VaccineScheduleTracking.API/Models/DTOs/UpdateAccountDto.cs
@@ -0,0 +1,13 @@
+﻿namespace VaccineScheduleTracking.API.Models.DTOs
+{
+    public class UpdateAccountDto
+    {
+        public int AccountID { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string? Avatar { get; set; }
+    }
+}
