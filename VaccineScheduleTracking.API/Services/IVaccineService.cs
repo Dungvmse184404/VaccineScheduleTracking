4b78b82dd49ae1172a0515d6a20136556baf6e2f
tulm <tulmse184390@fpt.edu.vn>
2025-01-29 08:44:59 +0700

    Add service layer for Vaccine

---
+++ VaccineScheduleTracking.API/Services/IVaccineService.cs
@@ -0,0 +1,10 @@
+﻿using VaccineScheduleTracking.API.Models.DTOs;
+using VaccineScheduleTracking.API.Models.Entities;
+
+namespace VaccineScheduleTracking.API.Services
+{
+    public interface IVaccineService
+    {
+        Task<List<Vaccine>> GetVaccinesAsync(FilterVaccineDto filterVaccineDto);
+    }
+}
---
+++ VaccineScheduleTracking.API/Services/VaccineService.cs
@@ -0,0 +1,21 @@
+﻿using VaccineScheduleTracking.API.Models.DTOs;
+using VaccineScheduleTracking.API.Models.Entities;
+using VaccineScheduleTracking.API.Repository;
+
+namespace VaccineScheduleTracking.API.Services
+{
+    public class VaccineService : IVaccineService
+    {
+        private readonly IVaccineRepository vaccineRepository;
+
+        public VaccineService(IVaccineRepository vaccineRepository)
+        {
+            this.vaccineRepository = vaccineRepository;
+        }
+
+        public async Task<List<Vaccine>> GetVaccinesAsync(FilterVaccineDto filterVaccineDto)
+        {
+            return await vaccineRepository.GetVaccinesAsync(filterVaccineDto); 
+        }
+    }
+}
