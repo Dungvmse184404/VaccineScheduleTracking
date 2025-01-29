01cdd58251df600195a7a0443af1a25aa4615bc6
tulm <tulmse184390@fpt.edu.vn>
2025-01-29 08:42:16 +0700

    Add Vaccine Class

---
+++ VaccineScheduleTracking.API/Models/Entities/Vaccine.cs
@@ -0,0 +1,17 @@
+﻿namespace VaccineScheduleTracking.API.Models.Entities
+{
+    public class Vaccine
+    {
+        public int VaccineID { get; set; }
+        public string Name { get; set; }
+        public int VaccineTypeID { get; set; }
+        public string Manufacturer { get; set; }
+        public int Stock {  get; set; }
+        public decimal Price { get; set; }  
+        public string Description { get; set; }
+        public int FromAge { get; set; }
+        public int ToAge { get; set; }
+        public int Period { get; set; }
+        public VaccineType VaccineType { get; set; }
+    }
+}
---
+++ VaccineScheduleTracking.API/Models/Entities/VaccineType.cs
@@ -0,0 +1,8 @@
+﻿namespace VaccineScheduleTracking.API.Models.Entities
+{
+    public class VaccineType
+    {
+        public int VaccineTypeID { get; set; }
+        public string Name { get; set; }
+    }
+}
