0072bbbd2e55bda43954ec96c04e9552bb9be7ce
tulm <tulmse184390@fpt.edu.vn>
2025-01-29 08:44:24 +0700

    Add request and respond class for search Vaccine

---
+++ VaccineScheduleTracking.API/Models/DTOs/FilterVaccineDto.cs
@@ -0,0 +1,12 @@
+﻿using VaccineScheduleTracking.API.Models.Entities;
+
+namespace VaccineScheduleTracking.API.Models.DTOs
+{
+    public class FilterVaccineDto
+    {
+        public string? Name { get; set; }
+        public string? Manufacturer { get; set; }
+        public int? FromAge { get; set; }
+        public string? VaccineType { get; set; }
+    }
+}
