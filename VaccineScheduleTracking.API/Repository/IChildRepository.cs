4bdcdfb43a292a30be8690a2c788424f9dce16b1
tulm <tulmse184390@fpt.edu.vn>
2025-02-02 13:27:02 +0700

    Create repository layer for Class Child

---
+++ VaccineScheduleTracking.API/Repository/IChildRepository.cs
@@ -0,0 +1,9 @@
+﻿using VaccineScheduleTracking.API.Models.Entities;
+
+namespace VaccineScheduleTracking.API.Repository
+{
+    public interface IChildRepository
+    {
+        Task<List<Child>> GetChildrenByParentID(int parentID);
+    }
+}
---
+++ VaccineScheduleTracking.API/Repository/SQLChildRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VaccineScheduleTracking.API.Data;
+using VaccineScheduleTracking.API.Models.Entities;
+
+namespace VaccineScheduleTracking.API.Repository
+{
+    public class SQLChildRepository : IChildRepository
+    {
+        private readonly VaccineScheduleDbContext dbContext;
+
+        public SQLChildRepository(VaccineScheduleDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<List<Child>> GetChildrenByParentID(int parentID)
+        {
+            return await dbContext.Children.AsQueryable().Where(x => x.ParentID == parentID).ToListAsync();
+        }
+    }
+}
