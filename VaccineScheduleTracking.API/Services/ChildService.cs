0228008eeb913c120d58e9cd0045a58ebbac310b
tulm <tulmse184390@fpt.edu.vn>
2025-02-02 13:27:32 +0700

    Create service layer for Class Child

---
+++ VaccineScheduleTracking.API/Services/ChildService.cs
@@ -0,0 +1,20 @@
+﻿using VaccineScheduleTracking.API.Models.Entities;
+using VaccineScheduleTracking.API.Repository;
+
+namespace VaccineScheduleTracking.API.Services
+{
+    public class ChildService : IChildService
+    {
+        private readonly IChildRepository childRepository;
+
+        public ChildService(IChildRepository childRepository)
+        {
+            this.childRepository = childRepository;
+        }
+
+        public async Task<List<Child>> GetParentChildren(int parentID)
+        {
+            return await childRepository.GetChildrenByParentID(parentID);
+        }
+    }
+}
