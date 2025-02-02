c3f3c374f0e1fed910dfb811f2f5b4a6f9878354
tulm <tulmse184390@fpt.edu.vn>
2025-02-02 08:16:26 +0700

    Add conditions for passwords

--- VaccineScheduleTracking.API/Models/DTOs/RegisterAccountDto.cs
+++ VaccineScheduleTracking.API/Models/DTOs/RegisterAccountDto.cs
@@ -1,14 +1,26 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace VaccineScheduleTracking.API.Models.DTOs
 {
     public class RegisterAccountDto
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-        public string? Avatar { get; set; }
+        [Required]
+        public string Username { get; set; } = string.Empty;
+        [Required]
+        [MinLength(8, ErrorMessage = "Password must have at least 8 characters.")]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*\W).{8,}$",
+        ErrorMessage = "Password must have at least 1 uppercase letter and 1 special character.")]
+        public string Password { get; set; } = string.Empty;
+        [Required]
+        public string Firstname { get; set; } = string.Empty;
+        [Required]
+        public string Lastname { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string? Avatar { get; set; } = string.Empty;
     }
 }
--- VaccineScheduleTracking.API/obj/Debug/net8.0/VaccineScheduleTracking.API.AssemblyInfo.cs
+++ VaccineScheduleTracking.API/obj/Debug/net8.0/VaccineScheduleTracking.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VaccineScheduleTracking.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f93f4ff5b02d4fa35fc446b307559ae81b5fd6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f4790c1e8cca2657c46c44675476684162e3504")]
 [assembly: System.Reflection.AssemblyProductAttribute("VaccineScheduleTracking.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VaccineScheduleTracking.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
