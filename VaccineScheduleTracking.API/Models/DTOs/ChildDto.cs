c522c49a1cdfc701d07f2adb9e8a6e2b0332cdb4
tulm <tulmse184390@fpt.edu.vn>
2025-02-02 13:28:04 +0700

    Create dto for Class Child

---
+++ VaccineScheduleTracking.API/Models/DTOs/ChildDto.cs
@@ -0,0 +1,13 @@
+﻿namespace VaccineScheduleTracking.API.Models.DTOs
+{
+    public class ChildDto
+    {
+        public int ChildID { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public decimal Weight { get; set; }
+        public decimal Height { get; set; }
+        public string Gender { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+    }
+}
