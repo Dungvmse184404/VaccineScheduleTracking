58f9ed2579ca8b14568296d908fab0800cc71455
tulm <tulmse184390@fpt.edu.vn>
2025-02-23 07:37:11 +0700

    Add data binding

--- VaccineScheduleTracking.API/Models/DTOs/Accounts/RegisterAccountDto.cs
+++ VaccineScheduleTracking.API/Models/DTOs/Accounts/RegisterAccountDto.cs
@@ -8,9 +8,9 @@ namespace VaccineScheduleTracking.API_Test.Models.DTOs.Accounts
         [Required]
         public string Username { get; set; } = string.Empty;
         [Required]
-        [MinLength(8, ErrorMessage = "Password must have at least 8 characters.")]
+        [MinLength(8, ErrorMessage = "Mật khẩu phải có ít nhất 8 kí tự.")]
         [RegularExpression(@"^(?=.*[A-Z])(?=.*\W).{8,}$",
-        ErrorMessage = "Password must have at least 1 uppercase letter and 1 special character.")]
+        ErrorMessage = "Mật khẩu phải có ít nhất 1 chữ viết hoa và 1 kí tự đặc biệt.")]
         public string Password { get; set; } = "";
         [Required]
         public string Firstname { get; set; } = string.Empty;
@@ -20,6 +20,7 @@ namespace VaccineScheduleTracking.API_Test.Models.DTOs.Accounts
         [EmailAddress]
         public string Email { get; set; } = string.Empty;
         [Required]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Số điện thoại phải có đúng 10 chữ số.")]
         public string PhoneNumber { get; set; } = string.Empty;
         public string? Avatar { get; set; } = string.Empty;
     }
--- VaccineScheduleTracking.API/Models/DTOs/Accounts/UpdateAccountDto.cs
+++ VaccineScheduleTracking.API/Models/DTOs/Accounts/UpdateAccountDto.cs
@@ -1,12 +1,18 @@
-﻿namespace VaccineScheduleTracking.API_Test.Models.DTOs.Accounts
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VaccineScheduleTracking.API_Test.Models.DTOs.Accounts
 {
     public class UpdateAccountDto
     {
         public int AccountID { get; set; }
+        [MinLength(8, ErrorMessage = "Mật khẩu phải có ít nhất 8 kí tự.")]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*\W).{8,}$",
+        ErrorMessage = "Mật khẩu phải có ít nhất 1 chữ viết hoa và 1 kí tự đặc biệt.")]
         public string? Password { get; set; }
         public string? Firstname { get; set; }
         public string? Lastname { get; set; }
         public string? Email { get; set; }
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Số điện thoại phải có đúng 10 chữ số.")]
         public string? PhoneNumber { get; set; }
         public string? Avatar { get; set; }
     }
