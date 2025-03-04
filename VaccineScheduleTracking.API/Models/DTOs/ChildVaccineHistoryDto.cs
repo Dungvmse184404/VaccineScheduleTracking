bfcb8a705530cc150b1957748d9df3094b345a94
tulm <tulmse184390@fpt.edu.vn>
2025-03-04 10:34:12 +0700

    Add more dto for VaccineRecord

---
+++ VaccineScheduleTracking.API/Models/DTOs/ChildVaccineHistoryDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VaccineScheduleTracking.API_Test.Models.DTOs
+{
+    public class ChildVaccineHistoryDto
+    {
+        [Required]
+        public int ChildID { get; set; }
+        [Required]
+        public int VaccineTypeID { get; set; }
+        public int? VaccineID { get; set; }
+        [Required]
+        public DateOnly Date { get; set; }
+        [Required]
+        public string? Note { get; set; }
+    }
+}
---
+++ VaccineScheduleTracking.API/Models/DTOs/CreateVaccineRecordDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VaccineScheduleTracking.API_Test.Models.DTOs
+{
+    public class CreateVaccineRecordDto
+    {
+        [Required]
+        public int ChildID { get; set; }
+        [Required]
+        public int VaccineTypeID { get; set; }
+        [Required]
+        public int VaccineID { get; set; }
+        [Required]
+        public int AppointmentID { get; set; }
+        [Required]
+        public DateOnly Date { get; set; }
+        [Required]
+        public string? Note { get; set; }
+    }
+}
