b1cb2be0c157cd11d6a4cef33061c209ca2ef48c
tulm <tulmse184390@fpt.edu.vn>
2025-03-05 19:30:40 +0700

    Create more dto for VaccineContainer

---
+++ VaccineScheduleTracking.API/Models/DTOs/DeleteVaccineContainerDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VaccineScheduleTracking.API_Test.Models.DTOs
+{
+    public class DeleteVaccineContainerDto
+    {
+        [Required]
+        public int VaccineComboID { get; set; }
+        [Required]
+        public int VaccineContainerID { get; set; }
+    }
+}
